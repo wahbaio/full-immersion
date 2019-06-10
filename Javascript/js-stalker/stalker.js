0a8cb5a96611396d4fc557d017e67ff3594de34a
Mohamed Mohamed <51259563+wahbaio@users.noreply.github.com>
2019-06-10 19:36:36 +0200

    Variables, prompt and console log
    Get as a input the name and age of a person to output them in the console log

---
+++ Javascript/js-stalker/stalker.js
@@ -0,0 +1,5 @@
+var name = prompt("What's your name?");
+var surname = prompt("What's your surname?");
+var age = prompt("How old are you?");
+console.log("Your name is "+name+" "+surname);
+console.log("Your age is "+age);
