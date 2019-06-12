5f4b660ec2cead112dd3bed321f2e81dfcd9f742
Mohamed Mohamed <51259563+wahbaio@users.noreply.github.com>
2019-06-12 20:18:29 +0200

    Use of if conditional to determine the case

---
+++ Javascript/conditionals/conditionals.js
@@ -0,0 +1,17 @@
+var age = prompt("How old are you?");
+
+if(age<0){
+    alert("Error, age must be a positive number");
+    } 
+
+if(age==21){
+    alert("happy 21st birthday!!");
+    } 
+
+if(age%2==1){
+    alert("your age is odd!");
+    } 
+
+if(age%Math.sqrt(age)==0){
+    alert("perfect square!");
+    }
