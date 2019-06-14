36f3102d2ed5cd1f2b7382c6713b46037f228e08
Mohamed Mohamed <51259563+wahbaio@users.noreply.github.com>
2019-06-14 20:29:08 +0200

    Learn to loop using for

---
+++ Javascript/for/for.js
@@ -0,0 +1,22 @@
+console.log("PRINTING ALL NUMBERS BETWEEN -10 AND 19");
+for(i=-10;i<20;i++){
+    console.log(i);
+}
+console.log("PRINTING ALL EVEN BETWEEN 10 AND 40");
+for(i=10;i<41;i++){
+    if(i%2===0){
+    console.log(i);
+    }
+}
+console.log("PRINTING ALL ODD NUMBERS BETWEEN 300 AND 333");
+for(i=300;i<334;i++){
+    if(i%2===1){
+    console.log(i);
+    }
+}
+console.log("PRINTING ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
+for(i=5;i<50;i++){
+    if(i%5===0&&i%3===0){
+    console.log(i);
+    }
+}
